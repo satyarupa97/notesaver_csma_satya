1ff8ca7d7ac537828f3b55e1dfb9549474de05c7
satyarupavathi daladuli <satyarupavathi.daladuli24@sasi.ac.in>
2026-07-14 09:23:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dns = require("dns")
-dns.setServers([8.8.8.8","8.8.4.4"])
+dns.setServers(["8.8.8.8","8.8.4.4"])
 const dotenv = require("dotenv");
 const cors = require("cors");
 const noteRoutes = require("./routes/noteRoutes");
